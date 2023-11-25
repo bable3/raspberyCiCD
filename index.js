8e5daf209ff7fb6ed8df1b7cc2a2790cb71738e6
FranckyFcn <francoisfaucon0987@gmail.com>
2023-11-25 15:23:09 +0100

    fix: tryy node app only

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.get("/", function (req, res) {
-  res.send("Hello francky !");
+  res.send("Hello michel !");
 });
 app.listen(8081, function () {
   console.log("app listening on port 8081!");
