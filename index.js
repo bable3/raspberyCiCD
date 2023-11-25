6bd156f57cc993803b527b7e19446da3637f2a3b
FranckyFcn <francoisfaucon0987@gmail.com>
2023-11-25 15:36:49 +0100

    feat: change to jean mich

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.get("/", function (req, res) {
-  res.send("Hello prout !");
+  res.send("Hello jean mich !");
 });
 app.listen(8081, function () {
   console.log("app listening on port 8081!");
